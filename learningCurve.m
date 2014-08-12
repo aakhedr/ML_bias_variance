fee0908ed2dd1e797e284387e44ec7cb38468a57
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-08-12 03:24:26 +0300

    final

--- learningCurve.m
+++ learningCurve.m
@@ -13,14 +13,8 @@ function [error_train, error_val] = ...
 %   dataset sizes from 1 up to m. In practice, when working with larger
 %   datasets, you might want to do this in larger intervals.
 %
-
 % Number of training examples
 m = size(X, 1);
-
-% You need to return these values correctly
-error_train = zeros(m, 1);
-error_val   = zeros(m, 1);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
 %               error_train and the cross validation errors in error_val. 
@@ -50,17 +44,13 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
-
 % ---------------------- Sample Solution ----------------------
+for i = 1:m
+	theta = trainLinearReg(X(1:i,:), y(1:i,:), lambda);
 
-
-
-
-
-
-
+	error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i,:), theta, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 % -------------------------------------------------------------
-
 % =========================================================================
-
 end
--- validationCurve.m
+++ validationCurve.m
@@ -15,7 +15,6 @@ lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 % You need to return these variables correctly.
 error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
 %               error_train and the validation errors in error_val. The 
@@ -37,17 +36,11 @@ error_val = zeros(length(lambda_vec), 1);
 %           
 %       end
 %
-%
+for i = 1:length(lambda_vec)
+	theta = trainLinearReg(X, y, lambda_vec(i));
 
-
-
-
-
-
-
-
-
-
+	error_train(i) = linearRegCostFunction(X, y, theta, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 % =========================================================================
-
 end
